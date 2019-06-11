03165c688d92a085ee20d15298f70682faa079c2
SeJin12 <tpwls8122@naver.com>
2019-06-11 12:14:35 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,16 +18,31 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2) // video : theme.spacing.unit * 2
   }
 })
+
+/* 실행 순서 Life Cycle
+  1) constructor()
+  2) componentWillMount()
+  3) render()
+  4) componentDidMount()
+*/
+/*
+  props or state => shouldComponentUpdate()
+*/
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({ customers: res }))
       .catch(err => console.log(err));
@@ -35,8 +51,12 @@ class App extends Component {
   callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
-    console.log('body ', body);
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
   }
 
   render() {
@@ -62,7 +82,12 @@ class App extends Component {
                     key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}
                   />
                 )
-              }) : ""
+              }) :
+                < TableRow >
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                  </TableCell>
+                </TableRow>
             }
           </TableBody>
         </Table>
